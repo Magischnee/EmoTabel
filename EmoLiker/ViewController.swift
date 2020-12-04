9fa3f6de436ecd26775c5895550e573a13b7e423
Maxim Vainikka <vainikka@users.noreply.github.com>
2020-12-04 20:28:48 +1000

    Initial Commit

---
+++ EmoLiker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmoLiker
+//
+//  Created by Максим on 04.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
